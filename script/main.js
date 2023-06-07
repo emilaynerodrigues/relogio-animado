16c0f8a7df01e595a135cc0fd656151f7ac9820e
Emilayne Rodrigues <63480241+emilaynerodrigues@users.noreply.github.com>
2023-06-07 19:38:04 -0400

    adicionando função para atualizar relogio

--- script/main.js
+++ script/main.js
@@ -1,14 +1,19 @@
 const dayContainer = document.querySelector(".day");
 
-document.addEventListener("DOMContentLoaded", () => {
-  new TypeIt(".dinamic", {
-    speed: 300,
-    loop: true,
-  })
-    .type("EU ESPAÇO", { delay: 900 })
-    .delete(6)
-    .go();
-});
+// seletores de hora, minuto, segundo
+const hoursContainer = document.querySelector(".hour");
+const minutesContainer = document.querySelector(".minute");
+const secondsContainer = document.querySelector(".second");
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   new TypeIt(".dinamic", {
+//     speed: 300,
+//     loop: true,
+//   })
+//     .type("EU ESPAÇO", { delay: 900 })
+//     .delete(6)
+//     .go();
+// });
 
 function showDay() {
   const weekDays = [
@@ -29,3 +34,21 @@ function showDay() {
 }
 
 setInterval(showDay, 1);
+function atualizarRelogio() {
+  // Obter a hora atual
+  const currentTime = new Date();
+
+  // Formatar a hora como uma string
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+
+  // Exibe a hora formatada
+  secondsContainer.textContent = seconds < 10 ? "0" + seconds : seconds; //se segundos for menor que 10 a função deve acrescentar o 0 na frente, senão continua normal
+  minutesContainer.textContent = minutes < 10 ? "0" + minutes : minutes;
+  hoursContainer.textContent = hours < 10 ? "0" + hours : hours;
+
+}
+
+// Atualizar o relógio a cada segundo
+setInterval(atualizarRelogio, 1000);
